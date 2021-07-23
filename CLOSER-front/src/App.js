28ee9d0d666d5130bda5c3db9a228d7dcddf3112
tjatn304905 <tjatn304905@gmail.com>
2021-07-23 15:49:27 +0900

    [ Fix ] App.js, index.js 에 뉴스피드 추가

--- CLOSER-front/src/App.js
+++ CLOSER-front/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import React, { useState } from 'react';
 import { Route, Link } from 'react-router-dom';
-import { Home, About, Login, SignUp, Profile } from './pages';
+import { Home, About, Login, SignUp, Profile, Newsfeed } from './pages';
 
 function App() {
   // const [ isLoggedIn, setIsLoggedIn ] = useState(false)
@@ -17,6 +17,9 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
+          <Link to="/newsfeed">뉴스피드</Link>
+        </li>
+        <li>
           <Link to="/login">로그인</Link>
         </li>
         <li>
@@ -28,15 +31,11 @@ function App() {
         <li>
           <Link to="/profile">프로필</Link>
         </li>
-        <li>
-          <Link to="/newsfeed">뉴스피드</Link>
-        </li>
       </ul>
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
       <Route path="/login" component={Login} />
-      {/* <router-link class="nav-link" @click.native="deleteJWT" to="#">Logout</router-link> */}
       <Route path="/signup" component={SignUp} />
       <Route path="/profile" component={Profile} />
       <Route path="/newsfeed" component={Newsfeed} />

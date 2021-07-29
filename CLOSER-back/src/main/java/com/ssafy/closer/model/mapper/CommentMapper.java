13adadddae02a40cc58c61634fe7148c31b9e9f4
조혜인 <choheyin1221@naver.com>
2021-07-29 11:41:53 +0900

    [ newfeed - modify ] delete 빼고피드 모든 기능 구현

--- CLOSER-back/src/main/java/com/ssafy/closer/model/mapper/CommentMapper.java
+++ CLOSER-back/src/main/java/com/ssafy/closer/model/mapper/CommentMapper.java
@@ -7,13 +7,13 @@ import java.util.List;
 public interface CommentMapper {
 
     // 해당 글의 댓글 리스트들
-    List<CommentDto> commentList();
+    List<CommentDto> commentList(CommentDto commentDto);
 
     // 댓글 생성
-    void addComment(CommentDto commentDto);
+    int createComment(CommentDto commentDto);
 
     // 댓글 삭제
-    void deleteComment(CommentDto commentDto);
+    int deleteComment(int comment_pk);
 
     // 본인이 쓴 댓글인지 확인 여부
     int isComment(CommentDto commentDto);
@@ -21,7 +21,6 @@ public interface CommentMapper {
     // 댓글 갯수
     int countComment(CommentDto commentDto);
 
-    // 해당 글이 삭제되면 관련된 댓글 삭제
-    void deleteAllComment(CommentDto commentDto);
-
+    // 해당 댓글을 쓴 유저 id 찾기
+    String findCommentUser(int comment_pk);
 }

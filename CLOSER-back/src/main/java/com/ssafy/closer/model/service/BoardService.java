3683a815dca51595b1ecba8d958da1281b344935
조혜인 <choheyin1221@naver.com>
2021-08-02 09:57:54 +0900

    [ develop - modify ] 주석해제

--- CLOSER-back/src/main/java/com/ssafy/closer/model/service/BoardService.java
+++ CLOSER-back/src/main/java/com/ssafy/closer/model/service/BoardService.java
@@ -5,22 +5,22 @@ import com.ssafy.closer.model.dto.BoardDto;
 import java.util.List;
 
 public interface BoardService {
-    public List<BoardDto> gBoardList(); // 자취 게시판 전체 인기글
-    public List<BoardDto> gBoardWeekBestList1(); // 자취 게시판 - 한끼레시피 한 주 인기글
-    public List<BoardDto> gBoardNewList1(); // 자취 게시판 - 한끼레시피 최신순
-    public List<BoardDto> gBoardBestList1(); // 자취 게시판 - 한끼레시피 인기글
+    List<BoardDto> gBoardList(); // 자취 게시판 전체 인기글
+    List<BoardDto> gBoardWeekBestList1(); // 자취 게시판 - 한끼레시피 한 주 인기글
+    List<BoardDto> gBoardNewList1(); // 자취 게시판 - 한끼레시피 최신순
+    List<BoardDto> gBoardBestList1(); // 자취 게시판 - 한끼레시피 인기글
 
-    public List<BoardDto> gBoardWeekBestList2(); // 자취 게시판 - 자취팁 한 주 인기글
-    public List<BoardDto> gBoardNewList2(); // 자취 게시판 - 자취팁 최신순
-    public List<BoardDto> gBoardBestList2(); // 자취 게시판 - 자취팁 인기글
+    List<BoardDto> gBoardWeekBestList2(); // 자취 게시판 - 자취팁 한 주 인기글
+    List<BoardDto> gBoardNewList2(); // 자취 게시판 - 자취팁 최신순
+    List<BoardDto> gBoardBestList2(); // 자취 게시판 - 자취팁 인기글
 
-    public List<BoardDto> gBoardWeekBestList3(); // 자취 게시판 - 홈데코 한 주 인기글
-    public List<BoardDto> gBoardNewList3(); // 자취 게시판 - 자취팁 최신순
-    public List<BoardDto> gBoardBestList3(); // 자취 게시판 - 자취팁 인기글
+    List<BoardDto> gBoardWeekBestList3(); // 자취 게시판 - 홈데코 한 주 인기글
+    List<BoardDto> gBoardNewList3(); // 자취 게시판 - 자취팁 최신순
+    List<BoardDto> gBoardBestList3(); // 자취 게시판 - 자취팁 인기글
 
-    public List<BoardDto> lBoardList1(String location); // 지역 게시판 - 공동구매 최신순
-    public List<BoardDto> lBoardList2(String location); // 지역 게시판 - 클로저모임 최신순
-    public List<BoardDto> lBoardList3(String location); // 지역 게시판 - 도와주세요 최신순
+    List<BoardDto> lBoardList1(String location); // 지역 게시판 - 공동구매 최신순
+    List<BoardDto> lBoardList2(String location); // 지역 게시판 - 클로저모임 최신순
+    List<BoardDto> lBoardList3(String location); // 지역 게시판 - 도와주세요 최신순
 
     boolean gBoardCreate(BoardDto boardDto); // gboard 생성
     boolean lBoardCreate(BoardDto boardDto); // lboard 생성

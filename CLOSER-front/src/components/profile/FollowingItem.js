9e139b95ecd464bdea8ebc47e2ac57a2a202fc4a
tjatn304905 <tjatn304905@gmail.com>
2021-08-04 20:15:55 +0900

    [ profile - feature ] 팔로잉, 팔로워 리스트 내 것에만 버튼 보이게

--- CLOSER-front/src/components/profile/FollowingItem.js
+++ CLOSER-front/src/components/profile/FollowingItem.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import { followAction } from '../../modules/user';
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 function FollowingItem({ following }) {
+
+  const dispatch = useDispatch();
+
+  const { userId } = useSelector((state) => state.user.userInfo)
+
+  // 팔로우 취소 버튼 클릭 시
+  const onClickFollow = () => {
+    axios.post(`http://localhost:8080/follow/${following.passiveUser}/follow`, {
+      userId: userId,
+      flag: 'true',
+    })
+    .then((res) => {
+      console.log(res)
+      dispatch(followAction())
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 
   return (
     <>
@@ -16,9 +37,16 @@ function FollowingItem({ following }) {
           <Col xs={4}>
             <Link to={`/profile/${following.passiveUser}`}>{following.nickname}</Link>
           </Col>
-          <Col xs={{ span: 3, offset: 3 }}>
-            <button>팔로우 취소</button>
-          </Col>
+          { following.activeUser === userId ?
+            <Col xs={6}>
+              <Row>
+                <Col xs={{ span: 6, offset: 6 }}>
+                  <button onClick={onClickFollow}>팔로우 취소</button>
+                </Col>
+              </Row>
+            </Col>
+            : <div></div>
+          }
         </Row>
       </Container>
     </>

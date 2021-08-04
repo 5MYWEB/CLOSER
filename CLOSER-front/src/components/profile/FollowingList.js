638ce942e720c9e17c10f0361c48ab82c6ae05d4
tjatn304905 <tjatn304905@gmail.com>
2021-08-04 13:59:32 +0900

    [ profile - feature ]팔로잉, 팔로워 컴포넌트 생성

--- CLOSER-front/src/components/profile/FollowingList.js
+++ CLOSER-front/src/components/profile/FollowingList.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux'; 
+import axios from 'axios';
 
-const FollowingList = ({header, data}) => {
-  // Profile.js 에서 받아온 객체 타입 data를 매핑해주는 코드입니다.
-  const Iteration = () => {
-    const followings = data.map(following => <li key={following.key}>{following.nickname}</li>);
-    return <ul>{followings}</ul>;
-  };
+import FollowingItem from './FollowingItem';
+
+const FollowingList = () => {
+
+  const { userId } = useSelector((state) => state.user.userInfo)
+
+  const [followingList, setFollowingList] = useState([])
+
+  useEffect(() => {
+    axios.post(`http://localhost:8080/follow/${userId}/following`)
+    .then((res) => {
+      setFollowingList(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
 
   return (
-    <div>
-      <div>{header}</div>
-      {Iteration()}
-    </div>
+    <>
+      <a href="javascript:history.back();">뒤로가기</a>
+
+      {followingList.length !== 0 ? 
+        <div>
+          {followingList.map((following) => {
+            return (
+              <FollowingItem key={following.follow_pk} following={following} />
+            );
+          })}
+        </div> :
+        <div>
+          아직 내가 팔로우하는 사람이 없습니다:(
+        </div> }
+    </>
   )
 };
 
-FollowingList.propTypes = {
-  header: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired,
-};
+
 
 export default FollowingList;
--- CLOSER-front/src/pages/Profile.js
+++ CLOSER-front/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux'
-// import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // import FollowerList from '../components/profile/FollowerList';
 // import FollowingList from '../components/profile/FollowingList';
@@ -12,7 +12,6 @@ import { useSelector } from 'react-redux'
 function Profile() {
 
   const { userInfo } = useSelector((state) => state.user);
-
 
   return (
     <div className="container">
@@ -31,7 +30,7 @@ function Profile() {
               {userInfo.nickname}
             </Col>
             <Col>
-              {userInfo.homeAlone}
+              자취 {userInfo.homeAlone}년차
             </Col>
           </Row>
           <Row>
@@ -48,15 +47,15 @@ function Profile() {
       </Row>
       {/* Row-4 : 한줄소개 */}
       <Row>
-        한줄소개
+        {userInfo.addr}
       </Row>
       {/* Row-5 : 팔로잉, 팔로워, 공유하기 */}
       <Row>
         <Col xs={3}>
-          팔로잉 컴포넌트, 라우터
+          <Link to={`/${userInfo.userId}/following-list`}>팔로잉</Link>
         </Col>
         <Col xs={3}>
-          팔로워 컴포넌트, 라우터
+          <Link to={`/${userInfo.userId}/follower-list`}>팔로워</Link>
         </Col>
         <Col xs={{ span: 3, offset: 3 }}>
           공유하기 버튼

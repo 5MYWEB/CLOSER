c11ffe647cd579fb1ba7f77e01dd04b33d432f05
minji1104 <minzy1104@gmail.com>
2021-08-12 00:40:38 +0900

    no message

--- CLOSER-front/src/components/profile/MyProfile.js
+++ CLOSER-front/src/components/profile/MyProfile.js
@@ -3,7 +3,19 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+<<<<<<< Updated upstream
 import UserBadge from './UserBadge';
+=======
+import backarrow from '../../assets/left-arrow.png';
+import userprofile from '../../assets/profile-user-demo.png';
+import './MyProfile.css';
+
+// import FollowerList from '../components/profile/FollowerList';
+// import FollowingList from '../components/profile/FollowingList';
+// import UserFeed from '../components/profile/UserFeed';
+// import UserPost from '../components/profile/UserPost';
+// import UserBookmark from '../components/profile/UserBookmark';
+>>>>>>> Stashed changes
 
 /* eslint-disable no-script-url */
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -36,13 +48,86 @@ function MyProfile() {
   }, [])
 
   return (
+<<<<<<< Updated upstream
     <Container>
+=======
+    <div className="container"> 
+>>>>>>> Stashed changes
       {/* Row-1 : 뒤로가기 */}
-      <Row>
-        <a href="javascript:history.back();">뒤로가기</a>
-      </Row>
+      <div className = "profilepart">
+        <div className = "item1">
+          <div className = "">
+            <a href="javascript:history.back();">
+              <img src={backarrow} alt="backarrow" className="backarrow" />
+            </a>
+          </div>
+        </div>
+
+        <div className = "item2">
+          <img src={userprofile} alt="userprofile" className="userprofile"/>
+        </div>
+
+        <div className = "item3">
+          {userInfo.nickname}
+        </div>
+
+        <div className = "item4">
+          {userInfo.homeAlone}년차
+        </div>
+
+        <div className = "item5">
+          <Link to="/profile-update">프로필 수정</Link>
+        </div>
+
+        <div className = "item6">
+          위치
+        </div>
+
+        <div className = "item7">
+          뱃지1
+        </div>
+        <div className = "item8">
+          뱃지2
+        </div>
+
+        <div className = "item9">
+          한줄소개
+        </div>
+
+        <div className = "item10">
+          <Link to={`/${userInfo.userId}/following-list`}>팔로잉 {followingListLength}</Link>
+        </div>
+
+        <div className = "item11">
+          <Link to={`/${userInfo.userId}/follower-list`}>팔로워 {followerListLength}</Link>
+        </div>
+
+        <div className = "item12">
+          프로필 공유
+        </div>
+      </div>
+
+      <div className="myprofilepost">
+        <div className ="myfeed">
+          <Link to={`/profile/${userInfo.userId}/user-feed`}>내 피드</Link>
+        </div>
+
+        <div className ="mypost">
+          <Link to={`/profile/${userInfo.userId}/user-board`}>내 포스트</Link>
+        </div>
+
+        <div className ="mybookmark">
+          <Link to={`/profile/${userInfo.userId}/user-bookmark`}>북마크</Link>
+        </div>
+
+
+      </div>
+  
+
+      {/* 프로필 사진 / 닉네임, 자취기간 // 위치 / 프로필 수정  */}
+
       {/* Row-2 : 프로필사진, 닉네임, 자취기간, 위치, 프로필 수정 */}
-      <Row>
+      {/* <Row>
         <Col xs={3}>
           <img src={userInfo.profileImg} alt="프로필 사진"></img>
         </Col>
@@ -62,16 +147,33 @@ function MyProfile() {
         <Col xs={3}>
           <Link to="/profile-update">프로필 수정</Link>
         </Col>
-      </Row>
+      </Row> */}
+
       {/* Row-3 : 뱃지 */}
+<<<<<<< Updated upstream
       <Row>
         <UserBadge userId={userInfo.userId}/>
       </Row>
+=======
+
+      {/* <Row>
+        뱃지 컴포넌트, 라우터
+      </Row> */}
+
+>>>>>>> Stashed changes
       {/* Row-4 : 한줄소개 */}
+{/* 
       <Row>
+<<<<<<< Updated upstream
         {userInfo.intro}
       </Row>
+=======
+        {userInfo.addr}
+      </Row> */}
+
+>>>>>>> Stashed changes
       {/* Row-5 : 팔로잉, 팔로워, 공유하기 */}
+{/* 
       <Row>
         <Col xs={2}>
           <Link to={`/${userInfo.userId}/following-list`}>팔로잉 {followingListLength}</Link>
@@ -82,9 +184,10 @@ function MyProfile() {
         <Col xs={{ span: 6, offset: 2 }}>
           공유하기 버튼
         </Col>
-      </Row>
+      </Row> */}
+      
       {/* Row-6 : 내피드, 내포스트, 북마크 */}
-      <Row>
+      {/* <Row>
         <Col>
           <Link to={`/profile/${userInfo.userId}/user-feed`}>내 피드</Link>
         </Col>
@@ -94,8 +197,17 @@ function MyProfile() {
         <Col>
           <Link to={`/profile/${userInfo.userId}/user-bookmark`}>북마크</Link>
         </Col>
+<<<<<<< Updated upstream
       </Row>
     </Container>
+=======
+      </Row> */}
+      
+      
+    </div>
+
+
+>>>>>>> Stashed changes
   );
 }
 

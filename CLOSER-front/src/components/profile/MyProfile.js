1bc881ca42f35618a1b04900cd16452e8ec894a6
minji1104 <minzy1104@gmail.com>
2021-08-12 10:50:12 +0900

    [feature/profiledesign] 프로필 css 1차 완성

--- CLOSER-front/src/components/profile/MyProfile.js
+++ CLOSER-front/src/components/profile/MyProfile.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import UserBadge from './UserBadge';
-import backarrow from '../../assets/left-arrow.png';
+import backarrow from '../../assets/arrow-left-solid.svg';
 import userprofile from '../../assets/profile-user-demo.png';
 import './MyProfile.css';
 
@@ -19,6 +19,9 @@ import './MyProfile.css';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 function MyProfile() {
+
+  const now = new Date();
+  const year = now.getFullYear;
 
   const { userInfo } = useSelector((state) => state.user);
 
@@ -50,11 +53,9 @@ function MyProfile() {
       {/* Row-1 : 뒤로가기 */}
       <div className = "profilepart">
         <div className = "item1">
-          <div className = "">
             <a href="javascript:history.back();">
               <img src={backarrow} alt="backarrow" className="backarrow" />
             </a>
-          </div>
         </div>
 
         <div className = "item2">
@@ -62,47 +63,121 @@ function MyProfile() {
         </div>
 
         <div className = "item3">
-          {userInfo.nickname}
+          <h4 className="userName"> {userInfo.nickname}</h4>
         </div>
 
         <div className = "item4">
-          {userInfo.homeAlone}년차
+          <h6 className="userYear"> {userInfo.homeAlone} 년차</h6>
         </div>
 
         <div className = "item5">
-          <Link to="/profile-update">프로필 수정</Link>
+          <button class = "animated-button">
+            <Link to="/profile-update" class="link-dark">프로필 수정</Link>
+          </button>
         </div>
 
         <div className = "item6">
-          위치
+          <h6 className="userLocation">사용자의 현재 위치</h6>
         </div>
 
         <div className = "item7">
-          뱃지1
+          <h6>요리왕</h6>
         </div>
         <div className = "item8">
-          뱃지2
+          <h6>살림왕</h6>
         </div>
 
         <div className = "item9">
-          한줄소개
+          <h6> 쌀국수 냉면 돈까스 좋아하는 자취생임</h6>
         </div>
 
         <div className = "item10">
-          <Link to={`/${userInfo.userId}/following-list`}>팔로잉 {followingListLength}</Link>
+          <h6><Link to={`/${userInfo.userId}/following-list`} class="link-dark">팔로잉 {followingListLength}</Link></h6>
         </div>
 
         <div className = "item11">
-          <Link to={`/${userInfo.userId}/follower-list`}>팔로워 {followerListLength}</Link>
+          <h6><Link to={`/${userInfo.userId}/follower-list`} class="link-dark">팔로워 {followerListLength}</Link></h6>
         </div>
 
         <div className = "item12">
-          프로필 공유
+          <button class = "animated-button">
+            <Link to="" class="link-dark">프로필 공유</Link>
+          </button>
         </div>
       </div>
 
       <div className="myprofilepost">
         <div className ="myfeed">
+          <h7><Link to={`/profile/${userInfo.userId}/user-feed`} class="link-light" >내 피드</Link></h7>
+        </div>
+
+        <div className ="mypost">
+          <Link to={`/profile/${userInfo.userId}/user-board`} class="link-light">내 포스트</Link>
+        </div>
+
+        <div className ="mybookmark">
+          <Link to={`/profile/${userInfo.userId}/user-bookmark`} class="link-light">북마크</Link>
+        </div>
+
+
+      </div>
+  
+
+      {/* 프로필 사진 / 닉네임, 자취기간 // 위치 / 프로필 수정  */}
+
+      {/* Row-2 : 프로필사진, 닉네임, 자취기간, 위치, 프로필 수정 */}
+      {/* <Row>
+        <Col xs={3}>
+          <img src={userInfo.profileImg} alt="프로필 사진"></img>
+        </Col>
+        <Col xs={6}>
+          <Row>
+            <Col>
+              {userInfo.nickname}
+            </Col>
+            <Col>
+              자취 {userInfo.homeAlone}년차
+            </Col>
+          </Row>
+          <Row>
+            {userInfo.addr}
+          </Row>
+        </Col>
+        <Col xs={3}>
+          <Link to="/profile-update">프로필 수정</Link>
+        </Col>
+      </Row> */}
+
+      {/* Row-3 : 뱃지 */}
+
+      {/* <Row>
+        뱃지 컴포넌트, 라우터
+      </Row> */}
+
+      {/* Row-4 : 한줄소개 */}
+{/* 
+      <Row>
+        {userInfo.addr}
+      </Row> */}
+
+      {/* Row-5 : 팔로잉, 팔로워, 공유하기 */}
+{/* 
+      <Row>
+        <Col xs={3}>
+          <Link to={`/${userInfo.userId}/following-list`}>팔로잉 {followingListLength}</Link>
+        </div>
+
+        <div className = "item11">
+          <Link to={`/${userInfo.userId}/follower-list`}>팔로워 {followerListLength}</Link>
+        </Col>
+        <Col xs={{ span: 3, offset: 3 }}>
+          공유하기 버튼
+        </Col>
+      </Row> */}
+      
+      {/* Row-6 : 내피드, 내포스트, 북마크 */}
+      {/* <Row>
+        <Col>
           <Link to={`/profile/${userInfo.userId}/user-feed`}>내 피드</Link>
         </div>
 
@@ -112,11 +187,12 @@ function MyProfile() {
 
         <div className ="mybookmark">
           <Link to={`/profile/${userInfo.userId}/user-bookmark`}>북마크</Link>
-        </div>
-
-      </div>
-
+        </Col>
+      </Row> */}
+      
+      
     </div>
+
 
   );
 }

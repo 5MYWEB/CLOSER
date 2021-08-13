a654c01e342c7dae9283c8ab5b5f06e9f9476305
tjatn304905 <tjatn304905@gmail.com>
2021-08-13 17:51:36 +0900

    [ design ] 사이드바 디자인 완성

--- CLOSER-front/src/components/profile/MyProfile.js
+++ CLOSER-front/src/components/profile/MyProfile.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState} from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import UserBadge from './UserBadge';
 import backarrow from '../../assets/arrow-left-solid.svg';
 import userprofile from '../../assets/profile-user-demo.png';
@@ -26,27 +24,9 @@ function MyProfile() {
   console.log(now);
   const year = now.getFullYear();
 
-  const [followingListLength, setFollowingListLength] = useState(0)
-  const [followerListLength, setFollowerListLength] = useState(0)
   const [annual, setAnnual] = useState(0)
-  // 팔로잉, 팔로워 수 가져오기
+
   useEffect(() => {
-    axios.post(`http://localhost:8080/follow/${userInfo.userId}/following`)
-    .then((res) => {
-      setFollowingListLength(res.data.length)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-    axios.post(`http://localhost:8080/follow/${userInfo.userId}/follower`)
-    .then((res) => {
-      setFollowerListLength(res.data.length)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
     setAnnual(year - userInfo.homeAlone + 1)
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -90,11 +70,11 @@ function MyProfile() {
         </div>
 
         <div className = "item10">
-          <h6><Link to={`/${userInfo.userId}/following-list`} class="link-dark">팔로잉 {followingListLength}</Link></h6>
+          <h6><Link to={`/${userInfo.userId}/following-list`} class="link-dark">팔로잉 {userInfo.following}</Link></h6>
         </div>
 
         <div className = "item11">
-          <h6><Link to={`/${userInfo.userId}/follower-list`} class="link-dark">팔로워 {followerListLength}</Link></h6>
+          <h6><Link to={`/${userInfo.userId}/follower-list`} class="link-dark">팔로워 {userInfo.follower}</Link></h6>
         </div>
 
         <div className = "item12">

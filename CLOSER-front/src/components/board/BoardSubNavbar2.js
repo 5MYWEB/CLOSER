e6aec3c9d86d32ebbe01a462a3b9b38a4a5da7bc
tjatn304905 <tjatn304905@gmail.com>
2021-08-19 13:18:39 +0900

    [ board - fix ] 게시판 네비게이션 바 오류 수정

--- CLOSER-front/src/components/board/BoardSubNavbar2.js
+++ CLOSER-front/src/components/board/BoardSubNavbar2.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeNavbar2 } from '../../modules/board';
 import { RippleTabItem } from '../../styles/index';
 import './BoardSubNavbar2.css';
 
 function BoardSubNavbar2({match, history}) {
 
-  const [nowTab, setNowTab] = useState(`/newsfeed/tip`)
+  const dispatch = useDispatch();
+
+  const { boardNavbar2 } = useSelector((state) => state.board)
 
   // 리플 탭 이동하는 주소
   const children = [
@@ -18,17 +22,18 @@ function BoardSubNavbar2({match, history}) {
   ]
 
   const onClickTap = ( e ) => {
-    setNowTab(e.target.getAttribute('addr'))
+    dispatch(changeNavbar2(e.target.getAttribute('addr')))
     history.replace(e.target.getAttribute('addr'))
   }
+
 
   return (
 
       <div className="tabs-wrapper2">
         <nav className="tabs">
-          <RippleTabItem cclass="tab" children={children[0][0]} onClick={onClickTap} addr={children[1][0]} />
-          <RippleTabItem cclass={ nowTab === children[1][1]? "tab is-current" : "tab"} children={children[0][1]} onClick={onClickTap} addr={children[1][1]} />
-          <RippleTabItem cclass={ nowTab === children[1][2]? "tab is-current" : "tab"} children={children[0][2]} onClick={onClickTap} addr={children[1][2]} />
+          <RippleTabItem cclass={ boardNavbar2 === children[1][0]? "tab is-current" : "tab"} children={children[0][0]} onClick={onClickTap} addr={children[1][0]} />
+          <RippleTabItem cclass={ boardNavbar2 === children[1][1]? "tab is-current" : "tab"} children={children[0][1]} onClick={onClickTap} addr={children[1][1]} />
+          <RippleTabItem cclass={ boardNavbar2 === children[1][2]? "tab is-current" : "tab"} children={children[0][2]} onClick={onClickTap} addr={children[1][2]} />
           <div className="nav-underline"></div> 
         </nav>
 
--- CLOSER-front/src/modules/board.js
+++ CLOSER-front/src/modules/board.js
@@ -7,6 +7,9 @@ const initialState = {
   boardUpdated: false,
   boardDeleted: false,
   boardLiked: false,
+  boardNavbar0: '/board/subnav1/',
+  boardNavbar1: '/board/subnav1/tip',
+  boardNavbar2: '/board/subnav2/getter',
 };
 
 /* 액션 타입 만들기 */
@@ -18,6 +21,9 @@ const GET_BEST_LIST = 'GET_BEST_LIST';
 const UPDATE_BOARD = 'UPDATE_BOARD';
 const DELETE_BOARD = 'DELETE_BOARD';
 const LIKE_BOARD = 'LIKE_BOARD';
+const CHANGE_NAVBAR0 = 'CHANGE_NAVBAR0';
+const CHANGE_NAVBAR1 = 'CHANGE_NAVBAR1';
+const CHANGE_NAVBAR2 = 'CHANGE_NAVBAR2';
 
 /* 액션 생성함수 만들기 */
 export const createBoard = () => {
@@ -72,6 +78,27 @@ export const likeBoard = () => {
   }
 };
 
+export const changeNavbar0 = (data) => {
+  return {
+    type: CHANGE_NAVBAR0,
+    data
+  }
+};
+
+export const changeNavbar1 = (data) => {
+  return {
+    type: CHANGE_NAVBAR1,
+    data
+  }
+};
+
+export const changeNavbar2 = (data) => {
+  return {
+    type: CHANGE_NAVBAR2,
+    data
+  }
+};
+
 
 /* 리듀서 선언 */
 const reducer = (state = initialState, action) => {
@@ -115,6 +142,21 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         boardLiked: !state.boardLiked,
+      };
+    case CHANGE_NAVBAR0:
+      return {
+        ...state,
+        boardNavbar0: action.data,
+      };
+    case CHANGE_NAVBAR1:
+      return {
+        ...state,
+        boardNavbar1: action.data,
+      };
+    case CHANGE_NAVBAR2:
+      return {
+        ...state,
+        boardNavbar2: action.data,
       };
     default:
       return state;
